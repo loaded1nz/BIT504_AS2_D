cdf7fe3c2c49fae44982d615ddb42e616593eaab
loaded1nz <59131151+loaded1nz@users.noreply.github.com>
2020-01-23 21:40:38 +1300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public  class  Main { 
+	static Scanner scanner = new Scanner(System.in); 
+	public  static  void  main(String[] args) {
+		system.out.println("My name is Scott");
+	}
+}
